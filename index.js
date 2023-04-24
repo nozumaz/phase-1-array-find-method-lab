709b1905db88cbf8e81980d6d4aedfac52aabf88
PK <nozumaz@gmail.com>
2023-04-24 06:47:57 -0400

    find win

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record) {
+
+    let findWin = record.find(({result}) => result === 'W')
+        if (findWin){
+            return findWin["year"]
+        }
+}
